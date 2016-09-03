9fa6ccb2eab8e9df73b30bd8648dd46578b2fe23
selvarajaguru <selvarajaguru.r@gmail.com>
2016-09-03 10:29:06 -0400

    all java files

---
+++ src/test/java/com/samplefinal/test/samplefinal/Documentation.java
@@ -0,0 +1,22 @@
+package com.samplefinal.test.samplefinal;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Ripon on 11/4/2015.
+ */
+public class Documentation {
+    private WebDriver driver;
+
+    public Documentation(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebDriverDocumentation navigateToWebDriverDocumentation(){
+        driver.findElement(By.linkText("Selenium WebDriver")).click();
+        return new WebDriverDocumentation(driver);
+    }
+}
